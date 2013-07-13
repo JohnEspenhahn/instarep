73e3e87eaef646de06e87a6ad32bb647fd643bab
JohnEspenhahn <johnshub99@gmail.com>
2013-07-13 10:42:31 -0400

    Fix search
    Minor fix:
    * error handling with 's'=>'sb' and 'h'=>'hb'
    * improve code readability

--- site/script.js
+++ site/script.js
@@ -53,6 +53,12 @@ function codeAddress() {
   });
 }
 
+function clearCookies() {
+	deleteCookie("ir_state");
+	deleteCookie("ir_district_House");
+	deleteCookie("ir_district_Senate");
+}
+
 ///////////////////////
 // Instarep Lookup
 ///////////////////////
@@ -83,21 +89,21 @@ function convertChamber(val) {
 function initWithLatLong() {
 	if (navigator.geolocation) {
 		element("name").innerHTML = "Loading...";
-		
+
 		var locationTimeout = window.setTimeout(function() { element("name").innerHTML = "Failed to geolocate your position."; }, 3333);
-		 
+
 		navigator.geolocation.getCurrentPosition(function(pos) {
 			clearTimeout(locationTimeout);
-			
+
 			var lat = pos.coords.latitude,
 				long = pos.coords.longitude;
-				
+
 			// Get state
 			if (checkCookie("ir_state") && checkCookie("ir_district_House") && checkCookie("ir_district_Senate")) {
 				var c_state = getCookie("ir_state"),
 					c_district_House = getCookie("ir_district_House"),
 					c_district_Senate = getCookie("ir_district_Senate");
-				
+
 				// Have both state and district, use them to lookup
 				getLegislatorFromStateAndDistricts(c_state, c_district_House, c_district_Senate);
 				element("state").value = c_state;
@@ -105,7 +111,7 @@ function initWithLatLong() {
 				element("district_Senate").value = c_district_Senate;
 			} else {
 				ajaxGoogleGetStateFromLatLong(lat, long);
-				
+
 				// Use latitude and longitude to find user's legislators
 				getLegislatorsFromLatLong(lat, long);
 			}
@@ -130,7 +136,7 @@ function ajaxGoogleGetStateFromLatLong(lat, long) {
 					break;
 				}
 			}
-			
+
 			if (!checkCookie("ir_state")) {
 				window.alert("Failed to find the state from your latitude and longitude.");
 			}
@@ -144,7 +150,7 @@ function getLegislatorsFromLatLong(lat, long) {
 	window.repsLoaded = false;
 	clearInfo();
 	window.docReps = [];
-	
+
 	element("name").innerHTML = "<table id='legInfo' border='1' cellpadding='5'><tr style='font-style: italic; background-color: " + color_yellow + ";'><td>Chamber</td><td>Party</td><td>Full Name</td><td>Email</td><td>Vote</td></tr></table>";
 	ajaxOpenStatesGetLegislatorsFromLatLong(lat, long);
 }
@@ -157,34 +163,34 @@ function ajaxOpenStatesGetLegislatorsFromLatLong(lat, long) {
 		success: function(json) {
 			var lng = json.length, val = null,
 				table = element("legInfo");
-				
+
 			window.docReps = [];
 			if (lng <= 0) {
 				element("name").innerHTML = "No representatives found.";
 				return;
 			}
-				
+
 			for(var i=json.length-1; i>=0; i--) {
 				val = json[i];
-				
+
 				if (!val.active)
 					continue;
 				else
 					convertChamber(val);
-				
+
 				// For search
 				var id = "rep" + val.leg_id;
 				window.docReps.push(id);
 				element("district_" + val.chamber).value = val.district;
 				setCookie("ir_district_" + val.chamber, val.district, 365);
-				
+
 				// Display
 				var row = table.insertRow(-1);
 				row.id = id + "_voteRow";
 				row.innerHTML = "<td>" + val.chamber + "</td><td>" + val.party + "</td><td>" + val.full_name + "</td>" +
 				    "<td><a href='mailto:" + val.email + "'>" + val.email + "</a></td><td id='" + id + "_vote'>N/A</td></tr>";
 			}
-			
+
 			checkDoneLoading();
 		},
 		error: function() { window.alert("Error connecting to Open States API."); }
@@ -197,13 +203,13 @@ function getLegislatorFromStateAndDistricts(state, house_district, senate_distri
 	clearInfo();
 	window.docReps = [];
 	element("name").innerHTML = "<table id='legInfo' border='1' cellpadding='5'><tr style='font-style: italic; background-color: " + color_yellow + ";'><td>Chamber</td><td>Party</td><td>Full Name</td><td>Email</td><td>Vote</td></tr></table>";
-	
+
 	setCookie("ir_state", state, 365);
 	element("state").value = state;
 
 	// House chamber (house)
 	ajaxOpenStatesGetHouse(state, house_district);
-	
+
 	// Senate chamber (senate)
 	ajaxOpenStatesGetSenate(state, senate_district);
 }
@@ -217,21 +223,21 @@ function ajaxOpenStatesGetHouse(state, house_district) {
 		success: function(json) {
 			var lng = json.length, val = null, 
 			    table = element("legInfo");
-				
+
 			for(var lowIt=0; lowIt<lng; lowIt++) {
 				val = json[lowIt];
-				
+
 				if (!val.active)
 					continue;
 				else
 					convertChamber(val);
-				
+
 				// For search
 				var id = "rep" + val.leg_id;
 				window.docReps.push(id);
 				element("district_House").value = val.district;
 				setCookie("ir_district_House", val.district, 365);
-				
+
 				// Display
 				var row = table.insertRow(-1);
 				row.id = id + "_voteRow";
@@ -254,28 +260,28 @@ function ajaxOpenStatesGetSenate(state, senate_district) {
 		success: function(json) {
 			var lng = json.length, val = null, 
 			    table = element("legInfo");
-			
+
 			for(var upIt=0; upIt<lng; upIt++) {
 				val = json[upIt];
-				
+
 				if (!val.active)
 					continue;
 				else
 					convertChamber(val);
-				
+
 				// For search
 				var id = "rep" + val.leg_id;
 				window.docReps.push(id);
 				element("district_Senate").value = val.district;
 				setCookie("ir_district_Senate", val.district, 365);
-				
+
 				// Display
 				var row = table.insertRow(-1);
 				row.id = id + "_voteRow";
 				row.innerHTML = "<td>" + val.chamber + "</td><td>" + val.party + "</td><td>" + val.full_name + "</td>" +
 				    "<td><a href='mailto:" + val.email + "'>" + val.email + "</a></td><td id='" + id + "_vote'>N/A</td></tr>";
 			}
-			
+
 			checkDoneLoading();
 		},
 		error: function() { window.alert("Error connecting to Open States API."); }
@@ -289,14 +295,14 @@ function checkDoneLoading() {
 
 	// Display and tell of update reps
 	window.repsLoaded = true;
-			
+
 	// Check for URL param
 	var results = new RegExp("[\\?&]bill=([^&#]*)").exec(window.location.search);
 	if (results != null) {
 		element("bill_id").value = decodeURIComponent(results[1]);
 		search();
 	}
-			
+
 	updateCookies();
 	clearInfo();
 
@@ -310,21 +316,15 @@ function clearInfo() {
 		var cellElmnt = element(window.docReps[i] + "_vote");
 		if (cellElmnt !== null)
 			cellElmnt.innerHTML = "N/A";
-			
+
 		// Set default row background color
 		var rowElmnt = element(window.docReps[i] + "_voteRow");
 		if (rowElmnt !== null)
 			rowElmnt.bgColor = "white";
 	}
-	
+
 	element("bill_desc").innerHTML = "";
 	element("other_bills").innerHTML = "";
-}
-
-function clearCookies() {
-	deleteCookie("ir_state");
-	deleteCookie("ir_district_House");
-	deleteCookie("ir_district_Senate");
 }
 
 // Update the cookies with the latest info
@@ -333,7 +333,7 @@ function updateCookies() {
 		state = element("state").value.toUpperCase(),
 		district_House = element("district_House").value,
 		district_Senate = element("district_Senate").value;
-		
+
 	if (getCookie("ir_state") != state) {
 		setCookie("ir_state", element("state").value.toUpperCase(), 365);
 		locChange = true;
@@ -357,7 +357,9 @@ function displayBills(jsonBillId) {
 
 	if (window.lastSearchJson.length > 1) {
 		element("other_bills").innerHTML = "<p><h6>Other matching bills:</h6><ul>";
-		for (var i=0; i<window.lastSearchJson.length - 1; i++) {
+		
+		var len = window.lastSearchJson.length - 1;
+		for (var i=0; i<len; i++) {
 			if (i == jsonBillId) {
 				element("other_bills").innerHTML += "<li><a href='javascript:displayBills(" + i + ");'><b>" + window.lastSearchJson[i].title + "</b></a></li>";
 			} else {
@@ -365,34 +367,37 @@ function displayBills(jsonBillId) {
 			}
 		}
 	}
-	
+
 	tableJson = window.lastSearchJson[jsonBillId];
 	// No vote yet
 	if (tableJson.votes.length <= 0) {
 		element("bill_desc").innerHTML = "<b>No votes</b> on <i>" + tableJson.title + " (" + tableJson.bill_id + ")</i>.";
 		displaySources(tableJson.sources);
-		return true;
 	} else {
 		// Convert from house/senate to house/senate
 		convertChamber(tableJson);
-	
+
 		var mss = "Latest vote on <i>" + tableJson.title + " (" + tableJson.bill_id + ")</i> of the " + tableJson.chamber + ". <br />"
 		element("bill_desc").innerHTML = mss
 		displaySources(tableJson.sources);
+		displayVotes(tableJson);
 	}
+}
 
+// Display the vote for each representative
+function displayVotes(tableJson) {
 	// Read votes
 	var votes = tableJson.votes[tableJson.votes.length-1],
 		yes_votes = votes.yes_votes, yes_count = yes_votes.length,
 		no_votes = votes.no_votes, no_count = no_votes.length,
 		other_votes = votes.other_votes, other_count = other_votes.length;
-	
+
 	// Read representatives
-	var docReps = document.getElementsByName("reps")[0].childNodes, lng = docReps.length, reps = new Array(lng);
+	var docReps = window.docReps, lng = docReps.length, reps = new Array(lng);
 	for (var i=0; i<lng; i++) {
-		reps[i] = { voteCellId: docReps[i].id + "_vote", voteRowId: docReps[i].id + "_voteRow", leg_id: docReps[i].value };
+		reps[i] = { voteCellId: docReps[i] + "_vote", voteRowId: docReps[i] + "_voteRow", leg_id: docReps[i].substring(3) }; // leg_id remove "rep"
 	}
-	
+
 	// Find representative's vote
 	var i, j, r, found;
 	for (i=0; i<lng; i++) {
@@ -407,7 +412,7 @@ function displayBills(jsonBillId) {
 			}
 		}
 		if (found) continue; // No need to do other loops
-		
+
 		for (j=0; j<no_count; j++) {
 			if (no_votes[j].leg_id == r.leg_id) {
 				element(r.voteCellId).innerHTML = "No";
@@ -417,7 +422,7 @@ function displayBills(jsonBillId) {
 			}
 		}
 		if (found) continue; // No need to do last loop
-		
+
 		for (j=0; j<other_count; j++) {
 			if (other_votes[j].leg_id == r.leg_id) {
 				element(r.voteCellId).innerHTML = "Absent";
@@ -426,20 +431,20 @@ function displayBills(jsonBillId) {
 			}
 		}
 	}
-	
-	// Print the sources of the result
-	function displaySources(sources) {
-		if (sources.length <= 0) return;
+}
 
-		var mss = "<h6> Sources: <br />";
-			
-		for (var i=0; i<sources.length; i++) {
-			mss += "<a target='_blank' href='" + sources[i].url + "'>" + sources[i].url.match(/\/\/(.+?)\//)[1] + "</a> <br />";
-		}
-		mss += "</h6>";
-		
-		element("sources").innerHTML = mss;
+// Print the sources of the result
+function displaySources(sources) {
+	if (sources.length <= 0) return;
+
+	var mss = "<h6> Sources: <br />";
+
+	for (var i=0; i<sources.length; i++) {
+		mss += "<a target='_blank' href='" + sources[i].url + "'>" + sources[i].url.match(/\/\/(.+?)\//)[1] + "</a> <br />";
 	}
+	mss += "</h6>";
+
+	element("sources").innerHTML = mss;
 }
 
 function search() {
@@ -449,36 +454,32 @@ function search() {
 	}
 
 	clearInfo();
-	
+
 	var bill_id = element("bill_id").value,
 		state = element("state").value.toUpperCase();
-		
+
 	// Update state and district cookies if different
 	updateCookies();
-	
+
 	element("bill_desc").innerHTML = "Loading <i>" + bill_id + "</i>...";
-	
+
 	// Remove old timer and add a new one
-	if (window.submitTimer != null) window.clearTimeout(window.submitTimer);
-	window.submitTimer = window.setTimeout( function() { element("bill_desc").innerHTML = "Load bill timed out. Are you sure you entered a valid bill?"; }, 5000);
-	
-	// Create the search string
-	var searchStr = "http://openstates.org/api/v1/bills/?state=" + state + "&search_window=session&q=" + bill_id + 
-		"&fields=votes,votes.yes_votes,votes.no_votes,votes.other_votes,title,bill_id,chamber,sources&apikey=18cdbb31b096462985cf408a5a41d3af";
-	
+	createNewTimeout(function() { element("bill_desc").innerHTML = "Load bill timed out. Are you sure you entered a valid bill?"; }, 5000);
+
 	// Submit the search string to open states
-	ajaxOpenStatesGetBill(searchStr);
+	ajaxOpenStatesGetBill(state, bill_id);
 }
 
-function ajaxOpenStatesGetBill(searchStr) {
+function ajaxOpenStatesGetBill(state, bill_id) {
 	$.ajax({
-		url: searchStr,
+		url: "http://openstates.org/api/v1/bills/?state=" + state + "&search_window=session&q=" + bill_id + 
+					"&fields=votes,votes.yes_votes,votes.no_votes,votes.other_votes,title,bill_id,chamber,sources&apikey=18cdbb31b096462985cf408a5a41d3af",
 		dataType: 'jsonp',
 		success: function(json) {
-			var lng = json.length, tableJson;
+			var lng = json.length;
 			if (lng <= 0) {
 				element("bill_desc").innerHTML = "No bill found with the search '" + bill_id + "'.";
-				
+
 				// Some error helping
 				if (bill_id.length > 1) {
 					var firstChar = bill_id.charAt(0).toUpperCase();
@@ -494,13 +495,21 @@ function ajaxOpenStatesGetBill(searchStr) {
 			} else {
 				window.clearTimeout(window.submitTimer);
 				window.lastSearchJson = json;
-				
+
 				// First first bill is the most recent
 				return displayBills(0);
 			}
 		},
 		error: function() { window.alert("Error connecting to Open States API."); }
 	});
+}
+
+///////////////////////
+// Util
+///////////////////////
+function createNewTimeout(func, timeout) {
+	if (window.submitTimer != null) window.clearTimeout(window.submitTimer);
+	window.submitTimer = window.setTimeout(func, timeout);
 }
 
 ///////////////////////
